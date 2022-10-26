e6c3dc31a02d1477118c415b5bf06e879e9d2bf4
tungnt24 <nguyentutung24@gmail.com>
2022-10-26 07:27:04 +0700

    update logic random proxies

--- src/emails/proxy/service.py
+++ src/emails/proxy/service.py
@@ -9,7 +9,7 @@ from src.config import Settings as settings
 
 
 def choice_random_email(db, account_id) -> Optional[ProxiesGenerated]:
-    proxies = db.query(VirtualUser).filter(VirtualUser.account_id==account_id).all()
+    proxies = db.query(VirtualUser).filter(VirtualUser.account_id==account_id, VirtualUser.used==False).all()
     domain = db.query(VirtualDomain).first()
     if proxies:
         proxy = random_proxy(proxies, domain, account_id, db)
@@ -20,21 +20,27 @@ def choice_random_email(db, account_id) -> Optional[ProxiesGenerated]:
         proxies = db.query(VirtualUser).filter(VirtualUser.account_id==account_id).all()
         proxy = random_proxy(proxies, domain, account_id, db)
         return ProxiesGenerated(email=proxy)
+
+n = -1
+def round_rob_seq(seq):
+  global n
+  n = n + 1
+  return seq[n % len(seq)]
         
     
 def random_proxy(proxies, domain, account_id, db):
     random_list = []
+    if len(proxies) < settings.LIMIT_PROXIES:
+        new_proxy = generate_email(db, domain, account_id)
+        random_list.append(new_proxy)
     for proxy in proxies:
-        if proxy.used:
-            new_proxy = generate_email(db, domain, account_id)
-            random_list.append(new_proxy)
         random_list.append(proxy.email)
-    proxy = choice(random_list)
+    proxy = round_rob_seq(random_list)
     return proxy
 
 
 def generate_email(db, domain, account_id):
-    username = "-".join(choice(settings.RANDOM_WORDS) for _ in range(2)).lower()
+    username = "_".join(choice(settings.RANDOM_WORDS) for _ in range(2)).lower()
     email_address = f"{username}@{domain.name}"
     proxy = VirtualUser(
         domain_id=domain.id,
